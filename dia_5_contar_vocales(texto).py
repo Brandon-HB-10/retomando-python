da97ff666dae60332384fa675feceeba5ef0947c
Bran.HB <brandonhernandezbriones456@gmail.com>
2026-08-08 23:57:18 -0600

    dia 5 contar vocales dependiendo de un texto

---
+++ dia_5_contar_vocales(texto).py
@@ -0,0 +1,8 @@
+def contar_vocales(texto):
+    vocales = 0
+    for letra in texto:
+        if letra in "aeiou":
+           vocales += 1
+    return vocales 
+            
+print(contar_vocales("Python"))
