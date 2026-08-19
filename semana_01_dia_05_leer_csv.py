545bfb773f59d422a89b6854f27c0db05a128889
Bran.HB <brandonhernandezbriones456@gmail.com>
2026-08-18 23:59:35 -0600

    dia 5: lectura y limpieza de datos csv

---
+++ semana_01_dia_05_leer_csv.py
@@ -0,0 +1,19 @@
+import csv
+
+vacantes_limpias = []
+with open("vacantes.csv", mode="r", encoding="utf-8") as archivo:
+    lector = csv.DictReader(archivo)
+    for vacante in lector:
+        try:
+            salario_convertido = int(vacante["salario"])
+            remoto_convertido = (vacante["remoto"]) ==  "True"
+            vacante_limpia = {
+    "puesto": vacante["puesto"].strip(),
+    "empresa": vacante["empresa"].strip(),
+    "salario": salario_convertido,
+    "remoto": remoto_convertido
+}
+            vacantes_limpias.append(vacante_limpia)
+        except ValueError:
+            continue
+print(vacantes_limpias)
